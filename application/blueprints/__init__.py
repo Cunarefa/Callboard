2b2a8593e9b499e41622b32f74c43d260f15d6eb
Yevgen <yevgen.yelik@gmail.com>
2021-10-15 18:10:34 +0300

    add posts

--- application/blueprints/__init__.py
+++ application/blueprints/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
 auth_api = Blueprint('auth_api', __name__)
+user_api = Blueprint('user_api', __name__)
+post_api = Blueprint('post_api', __name__)
 
-from application.blueprints import auth
+from application.blueprints import auth, users, posts
