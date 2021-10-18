21f06db1c117254dd634823812f9d5ed79629289
Yevgen <yevgen.yelik@gmail.com>
2021-10-18 17:02:02 +0300

    add pagination

--- application/blueprints/posts.py
+++ application/blueprints/posts.py
@@ -6,10 +6,12 @@ from application import db
 from application.blueprints import post_api
 from application.models.posts import PostSchema, Post
 from application.models import User
+from application.permissions import permission_required
 
 
 @post_api.route('/posts', methods=['POST'])
 @jwt_required()
+@permission_required(Post)
 def create_post():
     json_data = request.json
 
@@ -26,15 +28,9 @@ def create_post():
     return schema.dump(post), 201
 
 
-@post_api.route('/posts/<int:post_id>', methods=['GET'])
-def get_post(post_id):
-    post = db.session.query(Post).filter(Post.id == post_id).first_or_404()
-    schema = PostSchema()
-    return schema.dump(post), 200
-
-
 @post_api.route('/posts/<int:post_id>', methods=['DELETE'])
 @jwt_required()
+@permission_required(Post)
 def delete_post(post_id):
     post = db.session.query(Post).filter(Post.id == post_id).first_or_404()
     db.session.delete(post)
@@ -44,6 +40,7 @@ def delete_post(post_id):
 
 @post_api.route('/posts/<int:post_id>', methods=['PATCH'])
 @jwt_required()
+@permission_required(Post)
 def update_post(post_id):
     json_data = request.json
 
@@ -54,19 +51,33 @@ def update_post(post_id):
     except ValidationError as err:
         return abort(400, description=err)
 
-    db.session.query(Post).filter(Post.id == post_id).update(data)
+    db.session.query(Post).filter(Post.id == post.id).update(data)
     db.session.add(post)
     db.session.commit()
     return schema.dump(post)
 
 
 @post_api.route('/users/<int:user_id>/posts', methods=['GET'])
-@jwt_required()
-def posts_of_user(user_id):
-    posts = db.session.query(Post).join(User, User.id == Post.author_id).filter(User.id == user_id).all()
+def user_posts(user_id):
+    posts = db.session.query(Post).join(User, User.id == Post.author_id).filter(User.id == user_id)
+
+    priority = request.args.get('priority')
+    post_type = request.args.get('type')
+    page = request.args.get('page')
+
+    if post_type:
+        posts = posts.filter(Post.type == post_type)
+    if priority:
+        posts = posts.filter(Post.priority == priority)
+    if page:
+        posts = posts.paginate(page, 5, False).items
+
     schema = PostSchema(many=True)
     return jsonify(schema.dump(posts))
 
 
-
-
+@post_api.route('/posts/<int:post_id>', methods=['GET'])
+def get_post(post_id):
+    post = db.session.query(Post).filter(Post.id == post_id).first_or_404()
+    schema = PostSchema()
+    return schema.dump(post), 200
