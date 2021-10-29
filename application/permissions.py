6e673efa94ba94043e2ca5c2b821f1791de22884
Yevgen <yevgen.yelik@gmail.com>
2021-10-29 15:17:04 +0300

    modifying for diff models

--- application/permissions.py
+++ application/permissions.py
@@ -5,14 +5,14 @@ from flask import abort
 
 
 class permission_required:
-    def __init__(self, post):
-        self.post = post
+    def __init__(self, instance):
+        self.instance = instance
 
     def __call__(self, func):
         @wraps(func)
         def decorated(*args, **kwargs):
-            post = self.post.query.get(kwargs['post_id'])
-            if not current_user or (not current_user.is_admin and current_user.id != post.author_id):
+            instance = self.instance.query.get(kwargs['instance_id'])
+            if not current_user or (not current_user.is_admin and current_user.id != instance.author_id):
                 abort(403, 'Permissions denied')
             return func(*args, **kwargs)
 
